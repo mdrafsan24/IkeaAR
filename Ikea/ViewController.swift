fdc5e22debd48c070cd24b245c0f0fff90adc9d0
Rayan Slim <rayanslim@rayans-mbp.lan>
2017-08-18 15:21:36 +0300

    Initial Commit

---
+++ Ikea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ikea
+//
+//  Created by Rayan Slim on 2017-08-18.
+//  Copyright © 2017 Rayan Slim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
